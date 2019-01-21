6411d1c1465544b0cea6240c782f4c8da466ec31
Jake-Foster <35465022+Jake-Foster@users.noreply.github.com>
2019-01-21 15:11:46 -0500

    Added Source

---
+++ FizzBuzz/FizzBuzz.Library.Tests/FizzBuzzerTests.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Library.Tests
+{
+    [TestFixture]
+    public class FizzBuzzerTests
+    {
+        [Test]
+        public void Buzzer_WhenDefault_ReturnsInput(
+            [Values(1, 2, 4, 7, 8, 11, 13, 14,
+                    16, 17, 19)] int input)
+        {
+            // Arrange
+
+            // Act
+            string output = FizzBuzzer.GetValue(input);
+
+            // Assert
+
+            Assert.AreEqual(input.ToString(), output);
+        }
+
+        [Test]
+        public void Buzzer_WhenDiv3_ReturnsFizz(
+            [Values(3, 6, 9, 12, 18)] int input)
+        {
+            // Arrange
+
+            // Act
+            string output = FizzBuzzer.GetValue(input);
+
+            // Assert
+
+            Assert.AreEqual("Fizz", output);
+        }
+
+        [Test]
+        public void Buzzer_WhenDiv5_ReturnsBuzz(
+            [Values(5, 10, 20)] int input)
+        {
+            // Arrange
+
+            // Act
+            string output = FizzBuzzer.GetValue(input);
+
+            // Assert
+
+            Assert.AreEqual("Buzz", output);
+        }
+
+        [Test]
+        public void Buzzer_WhenDiv3andDiv5_ReturnsFizzBuzz(
+            [Values(15)] int input)
+        {
+            // Arrange
+
+            // Act
+            string output = FizzBuzzer.GetValue(input);
+
+            // Assert
+
+            Assert.AreEqual("FizzBuzz", output);
+        }
+    }
+}
